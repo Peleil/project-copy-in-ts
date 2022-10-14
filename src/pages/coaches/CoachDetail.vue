9d0820f821daee69e3ef10ca33b79a91a6edf249
Yusang Park <usang.park@gmail.com>
2022-10-14 13:40:41 -0700

    preset files deleted, main and route/index added

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>temaplate</h2>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,24 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+import CoachDetail from '../pages/coaches/CoachDetail.vue';
+import CoachesList from '../pages/coaches/CoachesList.vue';
+import CoachRegistation from '../pages/coaches/CoachRegistration.vue';
+import ContactCoach from '../pages/requests/ContactCoach.vue';
+import RequestsReceived from '../pages/requests/RequestsReceived.vue';
+import NotFound from '../pages/NotFound.vue';
 
 const routes: Array<RouteRecordRaw> = [
+  { path: '/', redirect: '/coaches' },
+  { path: '/coaches', component: CoachesList },
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/coaches/:id',
+    component: CoachDetail,
+    props: true,
+    children: [{ path: 'contact', component: ContactCoach }],
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+  { path: '/register', component: CoachRegistation },
+  { path: '/requests', component: RequestsReceived },
+  { path: '/:notFound(.*)', component: NotFound },
 ];
 
 const router = createRouter({
