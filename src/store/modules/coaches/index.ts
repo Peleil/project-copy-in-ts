57844a184f4282807dbea2f1122068e1efce7e7d
Yusang Park <usang.park@gmail.com>
2022-10-19 11:28:32 -0700

    files created without content

---
+++ src/store/modules/coaches/index.ts
@@ -0,0 +1,13 @@
+interface payload {}
+
+export default {
+  registerCoach(state, payload: payload) {
+    state.coaches.push(payload);
+  },
+  setCoaches(state, payload) {
+    state.coaches = payload;
+  },
+  setFetchTimestamp(state) {
+    state.lastFetch = new Date().getTime();
+  },
+};
